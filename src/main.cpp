bf452b5110f6d77572a3cf39fda28894edb1695d
cho45 <cho45@lowreal.net>
2016-02-23 09:37:03 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,35 @@ Adafruit_ADS1015 adc;
 // AD8307
 // Start 0.25V -74dBm
 // 25mV/dB
-//
-//
-// Direction Coupler: 26dB
-// Input Att: 16.1dB
+constexpr float AD9807_DBM_ORIGIN = -84.0; // 0.25/-74dBm => 0V/-84dBm
+constexpr float COUPLING_FACTOR = -29.54;
+constexpr float ATTENATOR_FACTOR = -16.1;
+
+constexpr float CALIB_A = 1.010544815;
+constexpr float CALIB_B = -102.0720562;
+
+// threshold (W) for not in transmit
+constexpr float TX_THRESHOLD_WATTS = 0.001;
+constexpr float TX_THRESHOLD_dBm = 10.0 * log10(TX_THRESHOLD_WATTS/1e-3);
+constexpr uint16_t TX_THRESHOLD_ADC = (TX_THRESHOLD_dBm + COUPLING_FACTOR + ATTENATOR_FACTOR - AD9807_DBM_ORIGIN) * 25;
+
+String formatWatts(float watts) {
+	if (watts < 1e-6) {
+		return String(watts * 1e6) + "uW";
+	} else
+	if (watts < 1e-3) {
+		return String(watts * 1e3) + "mW";
+	} else {
+		return String(watts) + "W";
+	}
+}
 
 void setup () {
 	Serial.begin(115200);
 	Serial.println("init");
+	Serial.print("TX_THRESHOLD_WATTS = "); Serial.println(TX_THRESHOLD_WATTS);
+	Serial.print("TX_THRESHOLD_dBm = "); Serial.println(TX_THRESHOLD_dBm);
+	Serial.print("TX_THRESHOLD_ADC = "); Serial.println(TX_THRESHOLD_ADC);
 
 	pinMode(13, OUTPUT);
 
@@ -24,11 +45,7 @@ void setup () {
 	adc.setGain(GAIN_ONE);
 }
 
-constexpr float AD9807_DBM_ORIGIN = -84.0; // 0.25/-74dBm => 0V/-84dBm
-constexpr float COUPLING_FACTOR = -26.02;
-constexpr float ATTENATOR_FACTOR = -16.1;
 
-constexpr float CALIB_A = 0.4036326942482341;
 
 void loop() {
 	digitalWrite(13, HIGH);
@@ -37,21 +54,24 @@ void loop() {
 	uint16_t adc_ref = adc.readADC_SingleEnded(1) * 2;
 	uint16_t adc_vcc = adc.readADC_SingleEnded(2) * 2;
 	uint16_t adc_gnd = adc.readADC_SingleEnded(3) * 2;
-
-//	uint16_t adc_fwd = adc.readADC_Differential_0_3_V() * 1000;
-//	uint16_t adc_ref = adc.readADC_Differential_1_3_V() * 1000;
-//	uint16_t adc_vcc = adc.readADC_Differential_2_3_V() * 1000;
-//	uint16_t adc_gnd = adc.readADC_SingleEnded(3) * 2;
+	if (adc_fwd < adc_ref) {
+		uint16_t tmp = adc_fwd;
+		adc_fwd = adc_ref;
+		adc_ref = tmp;
+	}
 
 	Serial.print("AIN fwd: "); Serial.println(adc_fwd);
 	Serial.print("AIN ref: "); Serial.println(adc_ref);
 	Serial.print("AIN vcc: "); Serial.println(adc_vcc);
 	Serial.print("AIN gnd: "); Serial.println(adc_gnd);
 
-	if (adc_fwd < adc_ref) {
-		uint16_t tmp = adc_fwd;
-		adc_fwd = adc_ref;
-		adc_ref = tmp;
+	adc_fwd = adc_fwd * CALIB_A + CALIB_B;
+	adc_ref = adc_ref * CALIB_A + CALIB_B;
+	if (adc_fwd < TX_THRESHOLD_ADC) {
+		Serial.println(" ");
+		digitalWrite(13, LOW);
+		delay(1000);
+		return;
 	}
 
 	float dbm_fwd = (float)adc_fwd / 25.0 + AD9807_DBM_ORIGIN - COUPLING_FACTOR - ATTENATOR_FACTOR;
@@ -61,17 +81,19 @@ void loop() {
 
 	float watts_fwd = pow(10, dbm_fwd / 10) / 1000.0;
 	float watts_ref = pow(10, dbm_ref / 10) / 1000.0;
-//	Serial.print("watts_fwd = "); Serial.println(watts_fwd);
+	Serial.print("watts_fwd = "); Serial.println(formatWatts(watts_fwd));
+	Serial.print("watts_ref = "); Serial.println(formatWatts(watts_ref));
+
+//	Serial.print("watts_fwd (W) = "); Serial.println(watts_fwd);
+//	Serial.print("watts_fwd (mW) = "); Serial.println(watts_fwd * 1000);
+//	Serial.print("watts_fwd (uW) = "); Serial.println(watts_fwd * 1000 * 1000);
 //	Serial.print("watts_ref = "); Serial.println(watts_ref);
 
-	watts_fwd = CALIB_A * watts_fwd;
-	watts_ref = CALIB_A * watts_ref;
-	Serial.print("watts_fwd (W) = "); Serial.println(watts_fwd);
-	Serial.print("watts_fwd (mW) = "); Serial.println(watts_fwd * 1000);
-	Serial.print("watts_fwd (uW) = "); Serial.println(watts_fwd * 1000 * 1000);
-	Serial.print("watts_ref = "); Serial.println(watts_ref);
+	float reflection_coefficient = sqrt(watts_ref / watts_fwd);
+	float swr = (1.0 + reflection_coefficient) / (1.0 - reflection_coefficient);
+	if (swr < 0) swr = 0/1; // invalid
 
 	Serial.println(" ");
 	digitalWrite(13, LOW);
-	delay(1000);
+	delay(500);
 }
